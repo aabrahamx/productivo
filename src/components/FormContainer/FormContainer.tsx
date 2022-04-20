da584d7b50c1880badbfa546c7e6206e4c61f0c8
Abel A <abelabrahamx@gmail.com>
2022-04-20 01:32:50 -0500

    add action change functionality

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
+import { ActionSelector } from "../ActionSelector/ActionSelector";
 import { CircleButton } from "../generic/Button/CircleButton";
 import "./FormContainer.css";
 
 export const FormContainer = (props: any) => {
   const [formType, setFormType] = useState(props.actionType);
-  const handleFormChange = ({ target }: any) => {
-    setFormType(target.value)
-  }
+  const handleFormChange = ({ target }: any): void => {
+    setFormType(target.value);
+  };
   return (
     <div className="container" id="container">
       <div className="form">
         <CircleButton onClick={props.handleClose}> x </CircleButton>
+        <ActionSelector
+          active={formType}
+          displayStyle="inline"
+          handleClick={handleFormChange}
+        />
         {props.children}
-        <p>{props.actionType}</p>
+        <p>{formType}</p>
       </div>
     </div>
   );
