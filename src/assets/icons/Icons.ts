b977533f1217ca9881dfeff0f0f90ada03d082a2
Abel A <abelabrahamx@gmail.com>
2022-07-10 17:15:32 -0500

    add: icon obj & imgs

---
+++ src/assets/icons/Icons.ts
@@ -0,0 +1,15 @@
+import sunIcon from './sun-white.png';
+import moonIcon from './moon-black.png';
+
+const icons = {
+  sun: {
+    src: sunIcon,
+    alt: 'Sun Icon',
+  },
+  moon: {
+    src: moonIcon,
+    alt: 'Moon Icon',
+  },
+};
+
+export default icons;
